dab122189a12efd3055488388b89607d32e08cc0
griffen.bradds@gmail.com <griffen.bradds@gmail.com>
2018-07-29 21:01:08 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -31,6 +31,6 @@ function accessElementInArray(array, index) {
   return array[index];
 }
 
-function accessElementInArray(array, index) {
+function destructivelyRemoveElementFromBeginningOfArray(array) {
   return array[index];
 }
