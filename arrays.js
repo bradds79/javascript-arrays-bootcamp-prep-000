bf437ef99106ea7b91478c3384342756fdc25dff
griffen.bradds@gmail.com <griffen.bradds@gmail.com>
2018-07-29 21:13:46 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -41,6 +41,11 @@ function destructivelyRemoveElementFromBeginningOfArray(array) {
   return array;
 }
 
+function removeElementFromBeginningOfArray(array) {
+  array = array.slice(1);
+  return array;
+}
+
 function destructivelyRemoveElementFromEndOfArray(array) {
   array.pop();
   return array;
